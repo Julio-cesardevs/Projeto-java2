c9a379f1edee80093db888d5ff22f5dc96634b19
cesar <103080100+chakaartt@users.noreply.github.com>
2024-06-05 21:30:48 -0300

    minha primera operaões.

---
+++ primeiro-projeto-java/src/estudovariaveis/Primeiraoperacoes.java
@@ -0,0 +1,14 @@
+package estudovariaveis;
+
+public class Primeiraoperacoes {
+	/*Main é método executável no Java*/
+	public static void main(String[] args) {
+		
+		int nota1 = 90;
+		int nota2 = 70;
+		int nota3 = 50;
+		int nota4 = 88;
+		
+		System.out.println("Media das notas = " + (nota1 + nota2 + nota3 + nota4) /4);
+	}
+}
